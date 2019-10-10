27cf24117a45ec37a87776ba6458200386f040e4
Julie <juliec@alleninstitute.org>
2019-10-10 16:26:21 -0700

    Add full param doctstrings to functions

--- normal_mode_analysis/mean_nuc_mesh_from_data.py
+++ normal_mode_analysis/mean_nuc_mesh_from_data.py
@@ -7,7 +7,6 @@ import vtk
 from skimage import io as skio
 from skimage import measure
 
-from matplotlib.path import Path
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
@@ -17,13 +16,12 @@ import meshcut
 import imageio
 import os
 
-from shapely.geometry import MultiPoint
-from shapely.geometry import Polygon
-from shapely.geometry import Point
-
 
 def create_csv_from_database():
-    """Download nuclear mesh databse with dsdb and save in repo."""
+    """Download nuclear mesh databse with dsdb and save in repo.
+        :return: dataframe containing downloaded timelapse nucleus segmentations.
+    """
+    
 	prod = dsdb.DatasetDatabase(config="~/.config_dsdb.json")
 	df = prod.get_dataset(id=304).ds
 	df.to_csv("mesh_vtk_files/nucleus_timelapse.csv")
@@ -32,10 +30,19 @@ def create_csv_from_database():
 
 
 def get_mask_from_mesh(polydata, imsize, dz):
-    """Given a polydata mesh, create a binary 3D mask, centered on the mask image center."""
+    """Given a polydata mesh, create a binary 3D mask, centered on the mask image center.
+    :param polydata: vtk 3D mesh
+    :param imsize: size of desired binary mask image
+    :param dz: spacing of z slices through mesh used to create mask
+    """
 
-	def get_cell_vert_array(i, polydata):
-        """Get vertices of nuclear mesh."""
+	def get_faces(i, polydata):
+    """Get vertices of nuclear mesh.
+    :param i: index of face in polydata mesh
+    :param polydata: mesh
+    :return: array of mesh faces , where each face is a list of indices of connected vertices
+    """
+        
 		cell = polydata.GetCell(i)
 		ids = cell.GetPointIds()
 		return np.array([ids.GetId(j) for j in range(ids.GetNumberOfIds())])
@@ -50,7 +57,10 @@ def get_mask_from_mesh(polydata, imsize, dz):
 	zmax = max(z_list)
 	
 	def get_zslice(n):
-        """Use meshcut to get z slices of mesh, create 2D masks of each slice, and combine slices to get 3D mask."""
+    """Use meshcut to get z slices of mesh, create 2D masks of each slice, and combine slices to get 3D mask.
+    :param n: index of z slice mask to cut from mesh
+    :return: 2D image as binary mask of z slice at this index
+    """
 
 		im = Image.fromarray(np.zeros((imsize,imsize), np.uint8).T)
 
@@ -60,7 +70,6 @@ def get_mask_from_mesh(polydata, imsize, dz):
 
 			draw = ImageDraw.Draw(im)
 			draw.polygon(polygon.round().astype(np.uint8).flatten().tolist(), fill=255)
-		#plt.imshow(im)
 
 		return np.array(im)/255
 
@@ -71,10 +80,19 @@ def get_mask_from_mesh(polydata, imsize, dz):
 
 	
 def get_mean_mask(df, imsize, dz):
-    """Given many meshes in df, find the average binary image mask of all meshes."""
+    """Given many meshes in df, find the average binary image mask of all meshes.
+    :param df: dataframe containing segmentation filepaths
+    :param imsize: size of binary mask image
+    :param dz: resolution of z slices
+    :return: 3D mean nuclear mask image array
+    """
 	
 	def get_mesh(i):
-        """Read database to get one mesh as polydata."""
+        """Read database to get one mesh as polydata.
+        :param i: index of segmentation in dataset
+        :return polydata mesh for this segmentation
+        """
+        
 		reader = vtk.vtkPolyDataReader()
         # read in a specific file
 		reader.SetFileName('mesh_vtk_files/'+df['CellId'][i]+'.vtk')
@@ -97,7 +115,10 @@ def get_mean_mask(df, imsize, dz):
 	
 	
 def get_mean_mesh(mask):
-    """Get mesh from mean mask, correcting z coordinates from zslice indexes to spatial values consistent with x/y."""
+    """Get mean mesh from mean mask, correcting z coordinates from zslice indexes to spatial values consistent with x/y.
+    :param mask: 3D binary image mask of average nuclear shape
+    :return: vertices and faces of mesh generated from mask
+    """
 
 	def fix_z(verts, dz, imsize):
 		nz = np.round(imsize/dz)
@@ -114,19 +135,28 @@ def get_mean_mesh(mask):
 	
     
 def get_mean_mesh_from_individual_meshes(df, imsize=200, dz=0.05):
-    """Master function, getting mean mesh shape from df of meshes."""
+    """Master function, getting mean mesh shape from df of meshes.
+    :param df: dataframe containing segmentation dataset
+    :param imsize: size of binary mask image
+    :param dz: resolution of z slices
+    :return: vertices and faces of average nuclear mesh
+    """
+    
 	mask = get_mean_mask(df, imsize, dz)
 	verts, faces = get_mean_mesh(mask)
-	np.save(verts,'nuc_verts')
-	np.save(faces, 'nuc_faces')
+    return verts, faces
 
 
-def plot_nuc_mask(mask, title, az):
-    """Plut mask in stripes on 3D axes."""
+def plot_nuc_mask(mask, title=None, az=None):
+    """Plut mask in stripes on 3D axes.
+    :param mask: 3D binary image mask of nucleus
+    :param title: title of file to save figure to, mostly used for genreating movie
+    """
 
 	fig = plt.figure()
 	ax = plt.axes(projection="3d")
-	ax.view_init(azim=az)
+    if az is not None:
+        ax.view_init(azim=az)
 
 	verts, faces, normals, values = measure.marching_cubes_lewiner(mask)
 	nverts = verts.shape[0]
@@ -135,11 +165,15 @@ def plot_nuc_mask(mask, title, az):
 	ax.plot_trisurf(x, y, faces, z, lw=0, cmap=plt.cm.Paired)
 
 	plt.tight_layout()
-	plt.savefig(title, format='png')
+    if title is not None:
+        plt.savefig(title, format='png')
 	
 	
 def make_nuc_video(mask, filename):
-    """Generate movie of striped mask rotating around z-axis."""
+    """Generate movie of striped mask rotating around z-axis.
+    :param mask: 3D binary image mask of nucleus
+    :param filename: name of file to save video to
+    """
     
 	images = []
 	for i in np.linspace(0, 360, 25):
