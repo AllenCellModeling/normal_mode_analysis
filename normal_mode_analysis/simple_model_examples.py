5c1720479db9768cab297751e444375120133f7b
Julie <juliec@alleninstitute.org>
2019-10-10 09:59:20 -0700

    Remove fully connected meshes to its own method for all cases

--- normal_mode_analysis/simple_model_examples.py
+++ normal_mode_analysis/simple_model_examples.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sb
 
+sb.set_palette(sb.color_palette("Set2"))
+color_list = sb.color_palette("Set2")*255
+
 
 # test model definitions: mass positions
 model_verts = {
@@ -34,60 +37,73 @@ model_faces = {
     '3D_th': [[0,1], [1,2], [2,0], [0,3], [1,3], [2,3]],
 }
 
-
-def nma_test_model(model, verts=None, faces=None):
-    
-    if model is not None:
-        verts = 10*model_verts[model]
-        faces = model_faces[model]
-    
-    hess = get_hessian_from_mesh(verts, faces)  
-    w, v = np.linalg.eigh(hess)
-    
-    d = np.matmul(np.linalg.inv(v),np.matmul(hess,v))
-    d[d<10**-5] = 0
-    np.real(d)
-    
-    for indpair in list(itertools.combinations(range(v.shape[0]), 2)):
-        a = indpair[0]
-        b = indpair[1]
-        c = np.dot(v[:,a], v[:,b])
-        if np.abs(c)<10**-5: c = 0 
-    
-    return verts, hess, w, v
-
-
-def nma_polygon(r, N=100):
-
-    def generate_circle_mesh(r, N):
-        verts = [np.array((math.cos(2*np.pi/N*x)*r, math.sin(2*np.pi/N*x)*r)) for x in range(0,N)]
-        faces = []
-        for i in range(N-1):
-            faces.append([i, i+1])
-        faces.append([N-1,0])
-        return verts, faces
-    
-    verts, faces = generate_circle_mesh(r, N)
-    
-    x = [verts[i][0] for i in range(len(verts))]
-    y = [verts[i][1] for i in range(len(verts))]
-    plt.scatter(x,y)
-    ax = plt.gca()
-    ax.set_aspect('equal')
-    plt.tight_layout()
-    
-    verts, hess, w, v = nma_test_model(None, np.array(verts), faces)
-    plt.figure()
-    sb.distplot(w, kde=False, bins=20)
-    draw_init_modes(verts, v, w)
-    
     
 def fully_connect_mesh(verts):
     return list(itertools.combinations(range(len(verts)),2))
 
 
-def draw_mode(verts, v2, axis=None):
+def check_diagonalization(v, hess):
     
+    d = np.matmul(np.linalg.inv(v),np.matmul(hess,v))
+    d[d<10**-5] = 0
+    return np.real(d)
+
+
+def check_orthogonality(v):
+    
+    for indpair in list(itertools.combinations(range(v.shape[0]), 2)):
+        a = indpair[0]
+        b = indpair[1]
+        c = np.dot(v[:,a], v[:,b])
+        if np.abs(c)<10**-5: c = 0
+    return np.sum(c) == 0
+
+
+def nma_test_model(model, verts=None, faces=None, fully_connect=False):
+    
+    if model is not None:
+        verts = model_verts[model]
+        faces = model_faces[model]
+        
+    if fully_connect:
+        faces = fully_connect_mesh(verts)
+            
+    if np.linalg.norm(verts==1):
+        verts *= 10
+    
+    hess = get_hessian_from_mesh(verts, faces)  
+    w, v = np.linalg.eigh(hess)
+    
+    return verts, faces, hess, w, v
+
+
+
+def nma_polygon(r, N=100, fully_connect=False, draw=False):
+
+    def generate_circle_mesh(r, N):
+        verts = [np.array((math.cos(2*np.pi/N*x)*r, math.sin(2*np.pi/N*x)*r)) for x in range(0,N)]
+        if not fully_connect:
+            faces = []
+            for i in range(N-1):
+                faces.append([i, i+1])
+            faces.append([N-1,0])
+        else:
+            faces = fully_connect_mesh(verts)
+        return verts, faces
+    
+    verts, faces = generate_circle_mesh(r, N)
+    
+    verts, faces, hess, w, v = nma_test_model(None, np.array(verts), faces, False)
+    
+    if draw:
+        sb.distplot(w, kde=False, bins=N*5)
+        plt.figure()
+        draw_init_modes(verts, faces, v, w)
+        
+    return w, v
+
+
+def draw_shape(verts, faces, c, axis=None):
     if axis is None:
         plt.figure()
         axis = plt.gca()
@@ -95,55 +111,49 @@ def draw_mode(verts, v2, axis=None):
     ndim = verts[0].shape[0]
         
     # get x (and y if ndim=2) coordinates for initial mass positions and draw initial shape
-    x = []
-    y = []
-    for i in range(verts.shape[0]):
-        x.extend([verts[i][0]])
+    for pair in faces:
+        x = [verts[pair[0]][0], verts[pair[1]][0]]
         if ndim==2:
-            y.extend([verts[i][1]])
-    x.extend([verts[0][0]])
-    if ndim==2:
-        y.extend([verts[0][1]])
-    else:
-        y = np.zeros(len(x))    
-    axis.plot(x,y)
+            y = [verts[pair[0]][1], verts[pair[1]][1]]
+        else:
+            y = np.zeros(len(x))
+        axis.plot(x,y, color=c)
+    return axis
 
+
+def draw_mode(verts, faces, v2, axis=None):
+    
+    nv = verts.shape[0]    
+    ndim = verts[0].shape[0]
+    axis = draw_shape(verts, faces, color_list[0], axis)
+
+    verts_new = []
     # get x (and y if ndim=2) displacements from eigenvectors and draw deformed shape
-    dx = []
-    dy = []
-    for i in range(verts.shape[0]):
-        dx.extend([v2[i]])
-        if ndim==2:
-            dy.extend([v2[i+verts.shape[0]]])
-    dx.extend([v2[0]])
-    if ndim==2:
-        dy.extend([v2[0+verts.shape[0]]])
-    else:
-        dy= np.zeros(len(dx))
+    for i in range(nv):
+        if ndim == 2:
+            verts_new.append([verts[i][0] + v2[i], verts[i][1] + v2[i + nv]])
+        else:
+            verts_new.append([verts[i][0] + v2[i]])
+    verts_new = np.asarray(verts_new)
+    axis = draw_shape(verts_new, faces, color_list[1], axis)
 
-    x2 = [sum(xx) for xx in zip(x, dx)]
-    y2 = [sum(yy) for yy in zip(y, dy)]
-    axis.plot(x2,y2)
-    axis.set_xlim(min(x2)-1,max(x2)+1)
-    axis.set_ylim(min(y2)-1,max(y2)+1)
     axis.set_aspect('equal')
 
     # draw eigenvectors as arrows from initial to new positions
-    for i in range(verts.shape[0]):
+    for i in range(nv):
 
         x = verts[i][0]
         dx = v2[i]
         if ndim==2:
             y = verts[i][1]
-            dy = v2[i+verts.shape[0]]
+            dy = v2[i+nv]
         else:
             y = 0
             dy = 0
         if dx!=0 or dy!=0:
-            axis.arrow(x,y,dx,dy, head_width=0.2)
+            axis.arrow(x,y,dx,dy, head_width=0.2, fc='k')
 
-
-def draw_init_modes(verts, v, w):
+def draw_init_modes(verts, faces, v, w):
     # calculate number of spatial dimensions and number of points
     ndim = verts[0].shape[0]
     npts = verts.shape[0]
@@ -151,7 +161,6 @@ def draw_init_modes(verts, v, w):
     # set up axes to plot all normal modes
     psize = 5
     fig, ax = plt.subplots(figsize=[psize*ndim,psize*npts], nrows=npts, ncols=ndim)
-    sb.set_palette(sb.color_palette("Set2"))
 
     # cycle through all normal modes (N = npts*ndim)
     for j in range(npts*ndim):
@@ -165,4 +174,5 @@ def draw_init_modes(verts, v, w):
         # set plot title to mode frequency
         axis.title.set_text('w = '+str(np.round(w[j],2)))
 
-        draw_mode(verts, v[:, j], axis)
+        axis = draw_mode(verts, faces, v[:, j], axis)
+
