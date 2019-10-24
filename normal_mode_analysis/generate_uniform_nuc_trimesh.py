a068ebda671e6ac061a72f618a07c2ddf391a791
Julie <juliec@alleninstitute.org>
2019-10-24 14:43:10 -0700

    Clean up for easy switching between nucleus and icosphere mesh generation

--- normal_mode_analysis/generate_uniform_nuc_trimesh.py
+++ normal_mode_analysis/generate_uniform_nuc_trimesh.py
@@ -14,29 +14,35 @@ import os
 import numpy as np
 
 # parameters
+mesh_type = 'nucleus' # set to either 'nucleus' or 'icosphere'
 mesh_density = 5 # 1-10 (but 10 creates like 5 million vertices)
 object_name = 'Mean Nuc Mesh' # blender sets mesh object name from input file name, so this should match
-#input_mesh_path = '/Users/blairl/Dropbox/AICS/VertexColoredMesh/mean_nuc_mesh.stl'
-#output_path = '/Users/blairl/Dropbox/AICS/VertexColoredMesh/mean_nuc_mesh_uniform_{}'.format(mesh_density)
 
 # replace "local_path" with your own repo's local path
 local_path = '/Users/juliec/projects'
 repo_path = '/normal_mode_analysis/normal_mode_analysis/nucleus_mesh_data/sample_trimeshes_from_blair/'
 input_mesh_path = local_path + repo_path + 'mean_nuc_mesh.stl'
-output_path = local_path + repo_path + 'icosphere_{}'.format(mesh_density)
-
+if mesh_type == 'nucleus':
+	output_path = local_path + repo_path + 'mean_nuc_mesh_uniform_{}'.format(mesh_density)
+elif mesh_type == 'icosphere':
+	output_path = local_path + repo_path + 'icosphere_{}'.format(mesh_density)
+else:
+	print('Please select "nucleus" or "icosphere" for your mesh_type. Defaulting to icosphere.')
+	output_path = local_path + repo_path + 'icosphere_{}'.format(mesh_density)
+	
 # delete default blender cube
 bpy.data.objects['Cube'].select_set(True)
 bpy.ops.object.delete()
     
 # import input mesh STL and center it 
 # (centering should probably be done earlier in pipeline, but doing it here for now)
-#bpy.ops.import_mesh.stl(filepath=input_mesh_path)
-#input_mesh = bpy.data.objects[object_name]
-#input_mesh.location.x = -100.
-#input_mesh.location.y = -100.
-#input_mesh.location.z = -100.
-#input_mesh.name = 'Input Mesh'
+if mesh_type == 'nucleus':
+	bpy.ops.import_mesh.stl(filepath=input_mesh_path)
+	input_mesh = bpy.data.objects[object_name]
+	input_mesh.location.x = -100.
+	input_mesh.location.y = -100.
+	input_mesh.location.z = -100.
+	input_mesh.name = 'Input Mesh'
 
 # create a sphere with icosahedral triangle mesh
 bpy.ops.mesh.primitive_ico_sphere_add(
@@ -49,14 +55,16 @@ bpy.ops.mesh.primitive_ico_sphere_add(
 
 # shrinkwrap sphere onto input mesh
 output_mesh = bpy.data.objects['Icosphere']
-#output_mesh.name = 'Output Mesh'
-#modifier = output_mesh.modifiers.new('shrinkwrap', 'SHRINKWRAP')
-#modifier.target = input_mesh
-#output_mesh.select_set(True)
-#bpy.ops.object.modifier_apply(apply_as='DATA', modifier='shrinkwrap')
+if mesh_type == 'nucleus':
+	output_mesh.name = 'Output Mesh'
+	modifier = output_mesh.modifiers.new('shrinkwrap', 'SHRINKWRAP')
+	modifier.target = input_mesh
+	output_mesh.select_set(True)
+	bpy.ops.object.modifier_apply(apply_as='DATA', modifier='shrinkwrap')
 
 # export STL file
-#input_mesh.select_set(False)
+if mesh_type == 'nucleus'
+	input_mesh.select_set(False)
 output_mesh.select_set(True)
 mesh_output_path = '{}.stl'.format(output_path)
 if os.path.exists(mesh_output_path):
